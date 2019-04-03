95149b441fb62ae014f0dab35e7c6da43f67bf51
Felix Vietmeyer <FeViTechLLC@gmail.com>
2019-04-03 05:45:03 -0600

    Formatting in parser

--- src/driver/vhdl_parser_driver.h
+++ src/driver/vhdl_parser_driver.h
@@ -18,22 +18,25 @@ public:
     virtual ~vhdl_driver ();
 
     int result;
+
     // Handling the scanner.
     void scan_begin ();
     void scan_end ();
     bool trace_scanning;
+
     // Run the parser on file F.
     // Return 0 on success.
     int parse (const std::string& f);
+
     // The name of the file being parsed.
     // Used later to pass the file name to the location tracker.
     std::string file;
+
     // Whether parser traces should be generated.
     bool trace_parsing;
+
     // Error handling.
     void error (const yy::location& l, const std::string& m);
     void error (const std::string& m);
-
-    //std::unique_ptr<AST::AstNode> top;
 };
 #endif // ! VHDL_DRIVER_HH
